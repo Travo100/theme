ac80766d2c2b1b65d3888a5e7b72502dc2c876b3
Matthew Roelle <matthewroelle@gmail.com>
2014-10-05 13:35:40 -0700

    Bare bones

---
+++ index.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700|Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+  <div class="social">
+    <div class="container">
+      <div class="col-sm-4">
+        <a href="tel:+1-555-555-5555"><i class="fa fa-phone-square"> +1-555-555-5555</i></a>
+      </div>
+      <div class="col-sm-4">
+        <a href="mailto:johndoe@gmail.com"><i class="fa fa-envelope"> johndoe@gmail.com</i></a>
+      </div>
+      <div class="col-sm-4">
+        <a href="http://facebook.com"><i class="fa fa-facebook-square"></i></a>
+        <a href="http://twitter.com"><i class="fa fa-twitter"></i></a>
+        <a href="http://instagram.com"><i class="fa fa-instagram"></i></a>
+        <a href="http://youtube.com"><i class="fa fa-youtube"></i></a>
+        <a href="http://google.com"><i class="fa fa-google-plus"></i></a>
+        <a href="http://pinterest.com"><i class="fa fa-pinterest"></i></a>
+        <a href="http://linkedin.com"><i class="fa fa-linkedin"></i></a>
+        <a href="http://github.com"><i class="fa fa-github"></i></a>
+      </div>
+    </div>
+  </div>
+    <div class="navbar navbar-inverse navbar-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Swoosh</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
