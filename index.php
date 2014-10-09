05c8e7d53a48eb3a1062f915293a8f6d2da1ef71
Matthew Roelle <matthewroelle@gmail.com>
2014-10-09 11:48:04 -0700

    Movable Blocks

--- index.php
+++ index.php
@@ -5,19 +5,24 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" type="text/css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./style.css" type="text/css">
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
     <title>Front-End Testing</title>
   </head>
   <body>
 <?php
 include('header.php');
+?> <div id="block-container"> <?php
 foreach(glob('blocks/*.php') as $block) {
   include($block);
 }
+?> </div> <?php
 include('footer.php');
 ?>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="./js/waypoints.min.js"></script>
     <script src="./js/main.js"></script>
+    <script src="./js/block-mover.js"></script>
   </body>
 </html>
---
+++ js/block-mover.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {  
+  $('#block-container').sortable();
+});
