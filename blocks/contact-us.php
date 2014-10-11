a375768b8ddff916c2b994e6200b856a57733ac8
Travis Thompson <Travo100@gmail.com>
2014-10-11 12:31:43 -0700

    Added footer.php and contact-us.php

---
+++ blocks/contact-us.php
@@ -0,0 +1,32 @@
+<div class="mt-contact-us">
+  <div class="container">
+    <h2>Contact Us</h2>
+    <div class="col-sm-4">
+      <i class="fa fa-mobile"></i>
+      <p>+1-555-555-5555</p>
+    </div>
+    <div class="col-sm-4">
+      <i class="fa fa-envelope-o"></i>
+      <p>johndoe@gmail.com</p>
+    </div>
+    <div class="col-sm-4">
+      <i class="fa fa-home"></i>
+      <address>555 Burrito Street</address>
+      <address>San Diego, CA 91121</address>
+    </div>
+    <div class="col-md-12">
+      <form action="mail.php" type="POST">
+        <div class="col-md-6">
+          <input type="text" name="name" class="form-control" placeholder="Your Name?">
+        </div>
+        <div class="col-md-6">
+          <input type="email" name="email" class="form-control" placeholder="Your Email?">
+        </div>
+        <div class="col-md-12">
+        <textarea name="message" id="message" class="form-control" placeholder="Your Message"></textarea>
+        <button type="submit" class="action-button">Send</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer>
+  <div class="container">
+    <div class="col-md-4">
+      <p>&copy;<?php echo date('Y');?> Swoosh &bull; Proudly powered by <a href="http://wordpress.org" target="_blank">WordPress</a></p>
+    </div>
+    <div class="col-md-4">
+      <div class="social-icons">
+        <a href="http://facebook.com"><i class="fa fa-facebook-square"></i></a>
+        <a href="http://twitter.com"><i class="fa fa-twitter"></i></a>
+        <a href="http://instagram.com"><i class="fa fa-instagram"></i></a>
+        <a href="http://youtube.com"><i class="fa fa-youtube"></i></a>
+        <a href="http://google.com"><i class="fa fa-google-plus"></i></a>
+        <a href="http://pinterest.com"><i class="fa fa-pinterest"></i></a>
+        <a href="http://linkedin.com"><i class="fa fa-linkedin"></i></a>
+        <a href="http://github.com"><i class="fa fa-github"></i></a>
+      </div>
+    </div>
+    <div class="col-md-4 nav-footer">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
