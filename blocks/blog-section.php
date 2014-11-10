1250ca9a6c0394a66064af9c29d36dd7e23c13b9
Travis Thompson <Travo100@gmail.com>
2014-11-09 19:20:22 -0800

    Added blog section block

---
+++ blocks/blog-section.php
@@ -0,0 +1,44 @@
+<div class="mt-block blog-section">
+	<div class="container">
+		<h2>Blog</h2>
+		<div class="row-blog">
+			<div class="col-md-2 col-md-offset-2 blog-img">
+				<a href="#">
+					<img src="http://placekitten.com/g/150/150">
+					<span>Read More</span>
+				</a>
+			</div> 
+			<div class="col-md-8 mt-text-blog">
+				<h3><a href="#">Check out this sweet blog post!</a></h3>
+				<h4>Posted by <a href="#">admin</a> on <?php echo date('F jS Y'); ?></h4>
+				<p>Donec vulputate nec massa ut maximus. Phasellus laoreet diam neque, ut blandit ante ullamcorper consectetur. Sed feugiat faucibus vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi sit amet risus nec eros venenatis laoreet. Cras sem mauris, tristique nec nulla et, laoreet aliquam sem. Duis et finibus lacus, in dignissim odio. Mauris eleifend rutrum suscipit. Nunc dapibus ac tellus ultricies rhoncus.</p>
+			</div>
+		</div>
+		<div class="row-blog">
+			<div class="col-md-2 col-md-offset-2 blog-img">
+				<a href="#">
+					<img src="http://placekitten.com/g/150/150">
+					<span>Read More</span>
+				</a>
+			</div> 
+			<div class="col-md-8 mt-text-blog">
+				<h3><a href="#">Check out this sweet blog post!</a></h3>
+				<h4>Posted by <a href="#">admin</a> on <?php echo date('F jS Y'); ?></h4>
+				<p>Donec vulputate nec massa ut maximus. Phasellus laoreet diam neque, ut blandit ante ullamcorper consectetur. Sed feugiat faucibus vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi sit amet risus nec eros venenatis laoreet. Cras sem mauris, tristique nec nulla et, laoreet aliquam sem. Duis et finibus lacus, in dignissim odio. Mauris eleifend rutrum suscipit. Nunc dapibus ac tellus ultricies rhoncus.</p>
+			</div>
+		</div>
+		<div class="row-blog">
+			<div class="col-md-2 col-md-offset-2 blog-img">
+				<a href="#">
+					<img src="http://placekitten.com/g/150/150">
+					<span>Read More</span>
+				</a>
+			</div> 
+			<div class="col-md-8 mt-text-blog">
+				<h3><a href="#">Check out this sweet blog post!</a></h3>
+				<h4>Posted by <a href="#">admin</a> on <?php echo date('F jS Y'); ?></h4>
+				<p>Donec vulputate nec massa ut maximus. Phasellus laoreet diam neque, ut blandit ante ullamcorper consectetur. Sed feugiat faucibus vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi sit amet risus nec eros venenatis laoreet. Cras sem mauris, tristique nec nulla et, laoreet aliquam sem. Duis et finibus lacus, in dignissim odio. Mauris eleifend rutrum suscipit. Nunc dapibus ac tellus ultricies rhoncus.</p>
+			</div>
+		</div>
+	</div>
+</div>
--- blocks/team-cards.php
+++ blocks/team-cards.php
@@ -1,44 +1,44 @@
-    <div class="mt-block team">
-      <div class="container">
-        <div class="col-md-4">
-          <div class="team-card mt-introduction mt-delay-2">
-            <div style="background-image: url(http://placekitten.com/320/240);" class="picture"></div>
-            <p class="name">Travis Thompson</p>
-            <p class="role">"Web" Developer</p>
-            <p class="desc">Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.</p>
-            <ul class="social">
-              <li><a href=""><i class="fa fa-github"></i></a></li>
-              <li><a href=""><i class="fa fa-twitter"></i></a></li>
-              <li><a href=""><i class="fa fa-envelope"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="team-card mt-introduction mt-delay-1">
-            <div style="background-image: url(http://placekitten.com/320/240);" class="picture"></div>
-            <p class="name">Travis Thompson</p>
-            <p class="role">"Web" Developer</p>
-            <p class="desc">Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.</p>
-            <ul class="social">
-              <li><a href=""><i class="fa fa-github"></i></a></li>
-              <li><a href=""><i class="fa fa-twitter"></i></a></li>
-              <li><a href=""><i class="fa fa-envelope"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="team-card mt-introduction mt-delay-2">
-            <div style="background-image: url(http://placekitten.com/320/240);" class="picture"></div>
-            <p class="name">Travis Thompson</p>
-            <p class="role">"Web" Developer</p>
-            <p class="desc">Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.</p>
-            <ul class="social">
-              <li><a href=""><i class="fa fa-github"></i></a></li>
-              <li><a href=""><i class="fa fa-twitter"></i></a></li>
-              <li><a href=""><i class="fa fa-envelope"></i></a></li>
-            </ul>
-          </div>
-        </div>
+<div class="mt-block team">
+  <div class="container">
+    <div class="col-md-4">
+      <div class="team-card mt-introduction mt-delay-2">
+        <div style="background-image: url(http://placekitten.com/320/240);" class="picture"></div>
+        <p class="name">Travis Thompson</p>
+        <p class="role">"Web" Developer</p>
+        <p class="desc">Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.</p>
+        <ul class="social">
+          <li><a href=""><i class="fa fa-github"></i></a></li>
+          <li><a href=""><i class="fa fa-twitter"></i></a></li>
+          <li><a href=""><i class="fa fa-envelope"></i></a></li>
+        </ul>
       </div>
     </div>
+    <div class="col-md-4">
+      <div class="team-card mt-introduction mt-delay-1">
+        <div style="background-image: url(http://placekitten.com/320/240);" class="picture"></div>
+        <p class="name">Travis Thompson</p>
+        <p class="role">"Web" Developer</p>
+        <p class="desc">Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.</p>
+        <ul class="social">
+          <li><a href=""><i class="fa fa-github"></i></a></li>
+          <li><a href=""><i class="fa fa-twitter"></i></a></li>
+          <li><a href=""><i class="fa fa-envelope"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="team-card mt-introduction mt-delay-2">
+        <div style="background-image: url(http://placekitten.com/320/240);" class="picture"></div>
+        <p class="name">Travis Thompson</p>
+        <p class="role">"Web" Developer</p>
+        <p class="desc">Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.Lorum ipsum dolor set amut. lorum ipsum dolor set amut. Lorum ipsum dolor set amut. Lorum ipsum dolor set amut.</p>
+        <ul class="social">
+          <li><a href=""><i class="fa fa-github"></i></a></li>
+          <li><a href=""><i class="fa fa-twitter"></i></a></li>
+          <li><a href=""><i class="fa fa-envelope"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
 
